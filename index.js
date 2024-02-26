56a94974e5d036b046678853ab94d7d93578762f
shrinija <2020bcs034@sggs.ac.in>
2024-02-27 05:24:07 +0530

    few changes in js file

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 function changeImageOnMobile() {
   // Get a reference to the image element
-  var myImage = document.getElementById('myImage');
+  var myImage = document.getElementById("myImage");
 
   // Check if the viewport width is less than or equal to 768 pixels (you can adjust this threshold)
   if (window.innerWidth <= 860) {
-      // Change the source of the image for mobile view
-      myImage.src = './topimgsmall.png';
+    // Change the source of the image for mobile view
+    myImage.src = "./topimgsmall.png";
   } else {
-      // Change the source of the image for desktop view
-      myImage.src = './topimage.png';
+    // Change the source of the image for desktop view
+    myImage.src = "./topimage.png";
   }
 }
 
-  // Call the function on page load and resize
-  changeImageOnMobile();
+// Call the function on page load and resize
+changeImageOnMobile();
 
-  // Attach the function to the window resize event to handle changes in viewport size
-  window.addEventListener('resize', changeImageOnMobile);
+// Attach the function to the window resize event to handle changes in viewport size
+window.addEventListener("resize", changeImageOnMobile);
 
-  
-function toggleMenu() {
-  const newnav = document.querySelector('.mobile-nav');
+//---------------------------------------------
 
-  if(newnav.classList.contains('mobile-nav')){
-    newnav.classList.remove("mobile-nav");
-    newnav.classList.add("new-links");
+const hamburger = document.querySelector(".hamburger");
+const mobileNav = document.querySelector(".mobile-nav");
+
+let isMobileNavOpen = false;
+
+hamburger.addEventListener("click", () => {
+  isMobileNavOpen = !isMobileNavOpen;
+  if (isMobileNavOpen) {
+    mobileNav.classList.remove("mobile-nav");
+    mobileNav.classList.add("new-links");
+  } else {
+    mobileNav.classList.remove("new-links");
+    mobileNav.classList.add("mobile-nav");
   }
-  else{
-    newnav.classList.remove("new-links");
-    newnav.classList.add("mobile-nav");
-  }
-}
+});
